7aebe724df9925ffb894d55ca100d46e01926ab8
iamkanishka <kanishkabc123@gmail.com>
2022-04-16 21:50:53 +0530

    Adding Auth Features State for the Auth Module in teh Angular NGRX Application

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { AUTH_STATE_NAME } from './State/auth.selector';
+import { AuthReducer } from './State/auth.reducer';
 
 
 
@@ -23,7 +26,8 @@ const routes: Routes = [
     CommonModule,
     RouterModule.forChild(routes),
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    StoreModule.forFeature(AUTH_STATE_NAME,AuthReducer)
 
 
   ]
