62749bac683802341a778cf91b358d05e5bec987
iamkanishka <kanishkabc123@gmail.com>
2022-04-17 07:46:03 +0530

    Save the User Token data from Login in NGRX State in Angular NGRX Application

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { environment } from 'src/environments/environment';
+import {AuthResponseData} from '../models/authresponse.model'
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
 
 
 @Injectable({
@@ -11,10 +14,16 @@ export class AuthService {
   constructor(private httpClient:HttpClient) { }
 
 
-  login(email:string, password:string){
+  login(email:string, password:string):Observable<AuthResponseData>{
  
-    return this.httpClient.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.FIREBASE_API_KEY}`,
+    return this.httpClient.post<AuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.FIREBASE_API_KEY}`,
     {email:email,password:password, returnSecureToken:true})
 
+  }
+
+  formatUser(data:AuthResponseData){
+    const expirationDate = new Date(new Date().getTime()+ +data.expiresIn*1000)
+    const user = new User(data.email,data.idToken,data.localId,expirationDate)
+    return user
   }
 }
--- src/app/auth/State/auth.effects.ts
+++ src/app/auth/State/auth.effects.ts
@@ -3,6 +3,7 @@ import { AuthService } from "src/app/Services/auth.service";
 import { loginStart, loginSuccess } from "./auth.action";
 import { exhaustMap, map } from 'rxjs/operators'
 import { Injectable } from "@angular/core";
+import { AuthResponseData } from "src/app/models/authresponse.model";
 
 @Injectable()
 export class AuthEffects {
@@ -10,8 +11,9 @@ export class AuthEffects {
     login$ = createEffect(() => {
 
         return this.action$.pipe(ofType(loginStart), exhaustMap((action) => {
-           return this.authservice.login(action.email, action.password).pipe(map((data)=>{
-               return loginSuccess();
+           return this.authservice.login(action.email, action.password).pipe(map((data:AuthResponseData)=>{
+               const user = this.authservice.formatUser(data)
+               return loginSuccess({user});
            }))
         }))
     })
