c782e426d28e2c1d1ac966ae30bf85e90a710a49
iamkanishka <kanishkabc123@gmail.com>
2022-04-24 22:34:24 +0530

    Ngrx Entity Configuration Sort Compare - NGRX

--- README.md
+++ README.md
@@ -100,3 +100,4 @@ So We will be Looking These Angular-NGRX  features and Learn as we go on.
 46. Implement CRUD Operations for Posts with NGRX Entity Adapter methods in Angular NGRX
 47. NGRX Entity Selector Adaptor Methods like Select All, Select Entities in Ngrx Angular Application 
 48. Remove unwanted Http API Calls i Data is Already Present in Ngrx Store - NGRX Application
+49. Ngrx Entity Configuration Sort Compare - NGRX 
--- src/app/posts/State/posts.state.entity.ts
+++ src/app/posts/State/posts.state.entity.ts
@@ -4,6 +4,28 @@ import { Post } from "src/app/models/posts.model"
 
 export interface PostsState extends EntityState<Post>{}
 
+// we can define the primary key for the Entity
+// export const postAdaptor =  createEntityAdapter<Post>({
+//     selectId: (post:Post)=>post.id
+// });
+
+//we can Sort varialbes  the primary key for the Entity
+// export function sortByName(a: Post, b: Post): number {
+//      const compare = a.title.localeCompare(b.title);
+//   if (compare > 0) {
+//     return -1;
+//   }
+
+//   if (compare < 0) {
+//     return 1;
+//   }
+
+//   return compare;
+//   }
+// export const postAdaptorsorted =  createEntityAdapter<Post>({
+//     sortComparer: sortByName,
+// });
+
 export const postAdaptor =  createEntityAdapter<Post>();
 
 export const initialState:PostsState =postAdaptor.getInitialState()
